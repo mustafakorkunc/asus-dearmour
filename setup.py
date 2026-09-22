84c0a9a2fea5a63fb0e2a2d483b9a0eddffcfaad
mustafakorkunc <mustafakorkunc@users.noreply.github.com>
2026-09-22 17:02:55 +0200

    fix(ci): fix pytest package discovery in CI and update metadata

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="asus-driver-extractor",
+    name="asus-dearmour",
     version="1.0.0",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
+            "dearmour=asus_driver_extractor.cli:main",
+            "dearmour-gui=asus_driver_extractor.gui:launch_gui",
             "asus-unpack=asus_driver_extractor.cli:main",
             "ade=asus_driver_extractor.cli:main",
             "ade-gui=asus_driver_extractor.gui:launch_gui",
